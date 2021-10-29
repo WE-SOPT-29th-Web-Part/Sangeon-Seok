5a1a9993f23975616daacdf9002f7856a936fbda
Bebop <tjr50999@naver.com>
2021-10-30 04:41:51 +0900

    Feat: [디데이 계산기 모든 기능 구현]

---
+++ d-day-calc/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import Header from "./components/Header";
+import DateInput from "./components/DateInput";
+import Result from "./components/Result";
+import Footer from "./components/Footer";
+import { useState } from "react";
+
+function App() {
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [date, setDate] = useState(new Date().getDate());
+
+  return (
+    <main id="home">
+      <section id="home__top">
+        <Header />
+        <DateInput
+          year={year}
+          month={month}
+          date={date}
+          setYear={setYear}
+          setMonth={setMonth}
+          setDate={setDate}
+        />
+      </section>
+      <div id="home__middle-line">D-Day 계산기</div>
+      <section id="home__bottom">
+        <Result year={year} month={month} date={date} />
+        <Footer />
+      </section>
+    </main>
+  );
+}
+
+export default App;
