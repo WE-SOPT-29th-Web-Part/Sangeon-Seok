f12d8fdd5eded469163d3c9e5986e8d01a383e08
Bebop <tjr50999@naver.com>
2021-11-04 13:18:05 +0900

    refactor: [props, state만을 사용해서 혼자 다시 만들어보기]

--- d-day-calc/src/App.js
+++ d-day-calc/src/App.js
@@ -1,34 +1,36 @@
 import "./App.css";
 import Header from "./components/Header";
 import DateInput from "./components/DateInput";
+import CenterText from './components/CenterText';
 import Result from "./components/Result";
 import Footer from "./components/Footer";
 import { useState } from "react";
 
+
 function App() {
   const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [month, setMonth] = useState(new Date().getMonth()+1);
   const [date, setDate] = useState(new Date().getDate());
 
   return (
-    <main id="home">
-      <section id="home__top">
-        <Header />
-        <DateInput
-          year={year}
-          month={month}
-          date={date}
-          setYear={setYear}
-          setMonth={setMonth}
-          setDate={setDate}
-        />
-      </section>
-      <div id="home__middle-line">D-Day 계산기</div>
-      <section id="home__bottom">
-        <Result year={year} month={month} date={date} />
-        <Footer />
-      </section>
-    </main>
+    <div id="app">
+      <Header />
+      <DateInput 
+      year={year}
+      month={month}
+      date={date} 
+      setYear={setYear}
+      setMonth={setMonth}
+      setDate={setDate}
+      />
+      <CenterText />
+      <Result 
+      year={year}
+      month={month}
+      date={date}
+      />
+      <Footer />
+    </div>
   );
 }
 
